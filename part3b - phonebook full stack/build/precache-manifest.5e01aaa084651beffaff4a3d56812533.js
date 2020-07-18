2ec2627f7e4ea3352cc58dacb9bf9ff93854e6eb
hmoilanen <juuso.ville.henrikki@gmail.com>
2020-07-18 14:26:49 +0300

    part 3.11 - done

---
+++ part3b - phonebook full stack/build/precache-manifest.5e01aaa084651beffaff4a3d56812533.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662f21167adfae21872e754778baf26b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3900c829561dd0db512",
+    "url": "/static/js/2.7d7ec4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d7ec4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f25423ced2d0cac5b9",
+    "url": "/static/js/main.36138534.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
